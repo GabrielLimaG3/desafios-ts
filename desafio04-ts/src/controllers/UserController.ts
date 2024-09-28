08643132e3b788e4d7996b6ee2b7bd97c0f80714
Gabriel <ga.lima2205@gmail.com>
2024-09-28 20:43:12 -0300

    init repo

---
+++ desafio04-ts/src/controllers/UserController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { UserService } from '../services/UserService'
+
+export class UserController {
+    userService: UserService
+
+    constructor(
+        userService = new UserService()
+    ){
+        this.userService = userService
+    }
+
+    createUser = (request: Request, response: Response): Response => {
+        const user = request.body
+
+        if(!user.name){
+            return response.status(400).json({ message: 'Bad request! Name obrigatório'})
+        }else if(!user.email){
+            return response.status(400).json({ message: 'Bad request! Email obrigatório'})
+        }
+
+        this.userService.createUser(user.name, user.email)
+        return response.status(201).json({ message: 'Usuário criado'})
+    }
+
+    getAllUsers = (request: Request, response: Response) => {
+        const users = this.userService.getAllUsers()
+        return response.status(200).json( users )
+    } 
+    DeleteUser = (request: Request, response: Response) => {
+        const user = request.body
+        console.log('Deletando usuário...', user)
+        return response.status(200).json({ message: 'Usuário deletado'})
+    }
+}
